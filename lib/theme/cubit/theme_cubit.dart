a20473301475e85d8b3ce329f25222070b9041f0
“Ayoola <omartins365@gmail.com>
2023-01-28 10:48:37 +0100

    presentation layer

--- lib/theme/cubit/theme_cubit.dart
+++ lib/theme/cubit/theme_cubit.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_weather/weather/weather.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
-import 'package:weather_repository/weather_repository.dart' show WeatherCondition;
 
 class ThemeCubit extends HydratedCubit<Color> {
   ThemeCubit() : super(defaultColor);
